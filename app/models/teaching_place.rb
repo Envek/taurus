271b146062676620e25e0ce1e56a5bf873c980a7
Novikov Andrey <envek@envek.name>
2011-06-21 12:32:56 +1000

    Changed relations in model, removed unnecessary method.

--- app/models/teaching_place.rb
+++ app/models/teaching_place.rb
@@ -3,7 +3,7 @@ class TeachingPlace < ActiveRecord::Base
   belongs_to :lecturer
   belongs_to :position
   has_many :charge_cards, :dependent => :destroy
-  has_many :assistant_charge_cards, :class_name => "ChargeCard"
+  has_many :assistant_charge_cards, :class_name => "ChargeCard", :foreign_key => "assistant_id"
 
   validates_presence_of :department, :lecturer
   validates_uniqueness_of :lecturer_id, :scope => :department_id
@@ -14,10 +14,6 @@ class TeachingPlace < ActiveRecord::Base
 
   def to_label
     lecturer.name
-  end
-  
-  def assistant_charge_cards
-    ChargeCard.all(:conditions => {'assistant_id' => self.id})
   end
   
 end
