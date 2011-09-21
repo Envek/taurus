6121d62f5f5deebd45af9d72825d74242abb916d
Novikov Andrey <envek@envek.name>
2011-09-21 14:41:28 +1000

    Added 'delete' links to faculties and departments in supervisor/faculties page

--- app/controllers/supervisor/faculties_controller.rb
+++ app/controllers/supervisor/faculties_controller.rb
@@ -1,5 +1,6 @@
 class Supervisor::FacultiesController < Supervisor::BaseController
    active_scaffold do |config|
+    config.actions << :delete
     config.columns = [:full_name, :name]
     config.nested.add_link('Кафедры', [:departments])
   end
