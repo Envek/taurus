0745824744eae794fb1339cee4d9cf70878f318a
Novikov Andrey <envek@envek.name>
2011-11-16 11:22:28 +1000

    A more little bit of optimization

--- app/controllers/editor/charge_cards_controller.rb
+++ app/controllers/editor/charge_cards_controller.rb
@@ -1,7 +1,7 @@
 class Editor::ChargeCardsController < Editor::BaseController
   def index
     charge_card_search = params[:charge_card].to_s.split
-    charge_card = ChargeCard.all
+    charge_card = ChargeCard.all(:select => "id,editor_name")
     charge_card_search.each do |s|
         charge_card = charge_card.select { |c| c.editor_name.include? s if c.editor_name}
     end
