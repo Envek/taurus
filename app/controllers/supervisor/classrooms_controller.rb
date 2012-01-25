2d5470a5f9a296acafe2870eb6a94f51ea44c158
Novikov Andrey <envek@envek.name>
2012-01-25 16:12:17 +1000

    Added possibility to delete classrooms by supervisor

--- app/controllers/supervisor/classrooms_controller.rb
+++ app/controllers/supervisor/classrooms_controller.rb
@@ -1,5 +1,6 @@
 class Supervisor::ClassroomsController < Supervisor::BaseController
   active_scaffold do |config|
+    config.actions << :delete
     config.columns = [:building, :department, :name, :capacity]
     config.columns[:building].form_ui = :select
     config.columns[:department].form_ui = :select
