140cce31ed35f0c4da070ca732c864efbf386604
Novikov Andrey <envek@envek.name>
2012-03-06 17:05:06 +1000

    Charge cards autogeneration based on teaching plans data

--- app/controllers/dept_head/specialities_controller.rb
+++ app/controllers/dept_head/specialities_controller.rb
@@ -7,6 +7,7 @@ class DeptHead::SpecialitiesController < DeptHead::BaseController
     config.nested.add_link('Группы', [:groups])
     config.action_links.add :teaching_plan, :label => "Учебный план", :type => :member, :page => true
     config.action_links.add :teaching_plan_import, :label => "Импорт учебного плана", :type => :collection, :page => true
+    config.action_links.add :add_charge_cards, :label => "Автосоздание карт нагрузки", :type => :member, :page => true
   end
 
   def teaching_plan
@@ -25,6 +26,36 @@ class DeptHead::SpecialitiesController < DeptHead::BaseController
       render "supervisor/teaching_plans/fill"
       return
     end
+  end
+
+  def add_charge_cards
+    @speciality = Speciality.find(params[:id])
+    discipline_ids = current_dept_head.department.disciplines.all(:select => "id").map{|d| d.id}
+    to_remove_ids = @speciality.groups.map{|g| g.jets}.flatten.map{|j| j.charge_card_id}.uniq
+    @cards_to_remove = ChargeCard.count(:conditions => {:id => to_remove_ids, :discipline_id => discipline_ids})
+  end
+
+  def create_charge_cards
+    @speciality = Speciality.find(params[:id])
+    discipline_ids = current_dept_head.department.disciplines.all(:select => "id").map{|d| d.id}
+    conditions = {:speciality_id => @speciality.id, :discipline_id => discipline_ids}
+    if params[:remove]
+      ids = @speciality.groups.map{|g| g.jets}.flatten.map{|j| j.charge_card_id}.uniq
+      deleted = (ChargeCard.destroy_all(:id => ids, :discipline_id => discipline_ids)).count
+    end
+    created = 0
+    @courses = TeachingPlan.all(:conditions => {:speciality_id => @speciality.id}, :select => "DISTINCT(course)").map{|p| p.course}.sort
+    @courses.each do |course|
+      groups = @speciality.groups.select{|g| g.course == course}
+      if groups.any?
+        conditions.merge!({:course => course, :semester => TAURUS_CONFIG["semester"]["current"]["number"]})
+        plans = TeachingPlan.all(:conditions => conditions)
+        plans.each do |plan|
+          created += (plan.create_charge_cards_for(groups)).count
+        end
+      end
+    end
+    redirect_to "/dept_head/specialities", :notice => "Создано карт нагрузок: #{created}#{", удалено: #{deleted}" if deleted}"
   end
 
   protected
