387f83f485f4efd37d2b3fc6163b8131b2bcb9b3
Novikov Andrey <envek@envek.name>
2012-03-27 13:38:45 +1000

    Added application autoversioning (based on current git repository tags).

---
+++ config/initializers/version.rb
@@ -0,0 +1,8 @@
+module Taurus
+  module Version
+    Described = (IO::popen('git describe --long') { |gs| gs.read }).strip
+    Number = Described.gsub('-', '.').gsub(/^v/, '').split('.')[0..-2].join('.')
+    Date = Date.strptime( IO::popen('git show -s --format="%ct"') { |gs| gs.read } , "%s")
+    Revision = (IO::popen('git show -s --format="%h"') { |gs| gs.read }).strip
+  end
+end
