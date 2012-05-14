0ee2d6b25d5377ad3fe0e643863534179203ee98
Novikov Andrey <envek@envek.name>
2012-05-14 22:44:07 +1000

    Let dept_head move charge cards between semesters

--- app/controllers/dept_head/charge_cards_controller.rb
+++ app/controllers/dept_head/charge_cards_controller.rb
@@ -1,17 +1,36 @@
 class DeptHead::ChargeCardsController < DeptHead::BaseController
   active_scaffold do |config|
     config.actions << :delete
-    config.columns = [:teaching_place, :assistant_teaching_place, :lesson_type, :jets, :discipline, :hours_quantity, :hours_per_week, :weeks_quantity, :groups]
+    config.columns = [:semester, :teaching_place, :assistant_teaching_place, :lesson_type, :jets, :discipline, :hours_quantity, :hours_per_week, :weeks_quantity, :groups]
     config.create.columns.exclude :groups, :hours_quantity
     config.update.columns.exclude :groups, :hours_quantity
     config.list.columns.exclude :jets
+    config.columns[:semester].form_ui = :select
+    config.columns[:semester].inplace_edit = true
     config.columns[:discipline].form_ui = :record_select
     config.columns[:lesson_type].form_ui = :select
+    config.columns[:lesson_type].inplace_edit = true
     config.columns[:teaching_place].form_ui = :select
     config.columns[:teaching_place].clear_link
+    config.columns[:teaching_place].inplace_edit = true
     config.columns[:assistant_teaching_place].form_ui = :select
     config.columns[:assistant_teaching_place].clear_link
+    config.columns[:assistant_teaching_place].inplace_edit = true
     config.columns[:discipline].clear_link
     config.columns[:groups].clear_link
+    config.columns[:hours_per_week].inplace_edit = true
+    config.columns[:weeks_quantity].inplace_edit = true
   end
+
+  def conditions_for_collection
+    {:semester_id => current_semester.id}
+  end
+
+protected
+
+  def do_new
+      super
+      @record.semester = current_semester
+  end
+
 end
