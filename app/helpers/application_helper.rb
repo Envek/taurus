f7887893c02f6aba58e31a94331641228ac470d8
Novikov Andrey <envek@envek.name>
2012-09-03 18:55:09 +1000

    Set current week number to be +1'd if 1st of September is Saturday.
    It's a hack. Just because you can't do anything with stohastic users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   def week_number (date=Date.today)
     semester_start_year = date.month < 9 ? date.year-1 : date.year
     semester_start = Date.strptime("#{semester_start_year}-09-01")
-    startweek_no = semester_start.cwday == 7 ? semester_start.cweek+1 : semester_start.cweek
+    startweek_no = semester_start.cwday.in?([6,7]) ? semester_start.cweek+1 : semester_start.cweek
     current_week = date.cweek + (date.year > semester_start_year ? 52 : 0)
     return 1 + ((current_week - startweek_no) % 2)
   end
