b498f4774c4a0ea92d2b386795bf5b36fc4b0eb4
Novikov Andrey <envek@envek.name>
2013-02-01 10:55:59 +1000

    Single menu for single user
    Redesign it using guest part menu

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -20,3 +20,10 @@ function formatResultedClassroomForSelect2 (result) {
 function formatSelectedClassroomForSelect2 (result) {
     return result.text.split(".")[0];
 }
+
+jQuery(document).ready(function($) {
+  $('menu>li.submenu>a').on('click', function() {
+    $('menu', $(this).parent()).first().slideToggle();
+    return false;
+  });
+});
