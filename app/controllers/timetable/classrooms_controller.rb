acfff3ff41c5bcc6d7356028c9c875de47ed332e
Novikov Andrey <envek@envek.name>
2013-03-21 11:32:51 +1000

    Allow to search available classrooms

--- app/controllers/timetable/classrooms_controller.rb
+++ app/controllers/timetable/classrooms_controller.rb
@@ -3,6 +3,13 @@ class Timetable::ClassroomsController < Timetable::BaseController
   def index
     @buildings = Building.includes(:classrooms).reorder("buildings.id,(substring(classrooms.name, '^[0-9]+'))::int,substring(classrooms.name, '[^0-9_].*$')")
     @buildings = @buildings.where("classrooms.name ILIKE ? OR classrooms.title ILIKE ? OR buildings.name ILIKE ?", *(["%#{params[:classroom]}%"]*3)) if params[:classroom]
+    if params[:day_of_the_week] and params[:week] and params[:pair_number]
+      week_filter = params[:week] == '0' ? [0,1,2] : [params[:week], 0]
+      unwanted_classroom_ids = Pair.where(week: week_filter, day_of_the_week: params[:day_of_the_week], pair_number: params[:pair_number]).pluck(:classroom_id).uniq.compact
+      if unwanted_classroom_ids.any?
+        @buildings = @buildings.where("classrooms.id NOT IN (#{unwanted_classroom_ids.join(",")})")
+      end
+    end
     respond_to do |format|
       format.html do
         if @buildings.count == 1 and @buildings.first.classrooms.count == 1
