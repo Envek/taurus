f9898479237afb18590a2ab4af64f19dd6178faf
Novikov Andrey <envek@envek.name>
2013-08-07 15:02:09 +1000

    Fix departments CRUD issue

--- app/controllers/supervisor/departments_controller.rb
+++ app/controllers/supervisor/departments_controller.rb
@@ -2,7 +2,8 @@
 class Supervisor::DepartmentsController < Supervisor::BaseController
   active_scaffold do |config|
     config.actions << :delete
-    config.columns = [:name, :short_name, :gosinsp_code, :charge_cards_all, :charge_cards_today]
+    config.columns = [:name, :short_name, :gosinsp_code]
+    config.list.columns = [:name, :short_name, :gosinsp_code, :charge_cards_all, :charge_cards_today]
     config.nested.add_link :specialities
     config.nested.add_link :teaching_places
     config.nested.add_link :disciplines
