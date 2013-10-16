74ea8995a0089b41c625921df1e9dd3a60612287
Novikov Andrey <envek@envek.name>
2013-10-16 13:04:12 +1000

    Teaching plans display enhancements

--- app/controllers/supervisor/specialities_controller.rb
+++ app/controllers/supervisor/specialities_controller.rb
@@ -10,10 +10,12 @@ class Supervisor::SpecialitiesController < Supervisor::BaseController
 
   def teaching_plan
     @speciality = Speciality.find(params[:id])
-    @teaching_plans = TeachingPlan.find_all_by_speciality_id(@speciality.id)
+    @teaching_plans = TeachingPlan.where(speciality_id: @speciality.id)
+    @courses = @teaching_plans.uniq.reorder(:course).pluck(:course)
+    @display_courses = params[:course].present? ? [params[:course].to_i] : @courses
+    @teaching_plans = @teaching_plans.where(course: params[:course]) if params[:course].present?
     discipline_ids = @teaching_plans.map{|tp| tp.discipline_id}.uniq
-    @disciplines = Discipline.find(discipline_ids, :order => :name)
-    @courses = @teaching_plans.map{|tp| tp.course}.uniq.sort
+    @disciplines = Discipline.reorder(:name).includes(:department).find(discipline_ids)
     render "application/specialities/teaching_plans/show"
   end
 
