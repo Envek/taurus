4de064f182475a76b276d592748aca954e40228c
Novikov Andrey <envek@envek.name>
2013-10-31 13:11:02 +0400

    Fix training assignments generation

--- app/controllers/concerns/specialities_teaching_plans_concern.rb
+++ app/controllers/concerns/specialities_teaching_plans_concern.rb
@@ -8,6 +8,7 @@ module SpecialitiesTeachingPlansConcern
     if defined? active_scaffold_config and active_scaffold_config
       active_scaffold_config.action_links.add :teaching_plan, :label => "Учебный план", :type => :member, :page => true
       active_scaffold_config.action_links.add :teaching_plan_import, :label => "Импорт учебного плана", :type => :collection, :page => true
+      active_scaffold_config.action_links.add :generate_training_assignments, type: :collection, method: :post, page: true
     end
   end
 
@@ -35,6 +36,56 @@ module SpecialitiesTeachingPlansConcern
       return
     end
     render "application/specialities/teaching_plans/new"
+  end
+
+  protected
+
+  def create_training_assignments
+    created = 0
+    speciality_ids = params[:id].present?? params[:id] : marked_records.to_a
+    courses = params[:course].present?? [params[:course].to_i] : Group.where(speciality_id: speciality_ids).map(&:course).uniq.sort
+    teaching_plans = TeachingPlan.where(speciality_id: speciality_ids, semester: current_semester.number)    
+    TrainingAssignment.transaction do
+      courses.each do |course|
+        forming_year = current_semester.year - course + 1
+        course_teaching_plans = teaching_plans.where(course: course, forming_year: forming_year)
+        discipline_ids = course_teaching_plans.uniq.pluck(:discipline_id)
+        disciplines = Discipline.reorder(:name).includes(:department).find(discipline_ids)
+        groups = Group.where(speciality_id: speciality_ids, forming_year: forming_year)
+        disciplines.each do |discipline|
+          # Lections
+          entries = course_teaching_plans.where(discipline_id: discipline.id).where("teaching_plans.lections IS NOT NULL")
+          entries.group_by{|entry| entry.lections }.each do |hours, plan_entries|
+            group_ids = plan_entries.map(&:group_ids).flatten.uniq
+            conditions = {weeks_quantity: 18, hours: hours, lesson_type_id: 1, semester_id: current_semester.id, groups: {id: group_ids}, disciplines: {id: [discipline.id]}}    
+            lec = TrainingAssignment.where(conditions).first_or_initialize
+            unless lec.persisted?
+              lec.group_ids = group_ids
+              lec.disciplines = [discipline]
+              lec.save and (created += 1)
+            end
+          end
+          # Practics and laboratory works
+          [[:practics, 2], [:lab_works, 3]].each do |type, type_id|
+            entries = course_teaching_plans.where(discipline_id: discipline.id).where("teaching_plans.#{type} IS NOT NULL")
+            entries.group_by{|entry| entry.send(type) }.each do |hours, plan_entries|
+              group_ids = plan_entries.map(&:group_ids).flatten.uniq
+              group_ids.each do |group_id|
+                conditions = {weeks_quantity: 18, hours: hours, lesson_type_id: type_id, semester_id: current_semester.id, groups: {id: group_id}, disciplines: {id: [discipline.id]}}    
+                lec = TrainingAssignment.where(conditions).first_or_initialize
+                unless lec.persisted?
+                  lec.group_ids = [group_id]
+                  lec.disciplines = [discipline]
+                  lec.save and (created += 1)
+                end
+              end
+            end
+          end
+        end
+      end
+      each_marked_record {|r| r.as_marked = false} if marked_records.any?
+    end
+    created
   end
 
 end
--- app/controllers/faculty/specialities_controller.rb
+++ app/controllers/faculty/specialities_controller.rb
@@ -7,47 +7,13 @@ class Faculty::SpecialitiesController < Faculty::BaseController
     config.nested.add_link :groups
     config.columns[:department].form_ui = :select
     config.actions.add :mark
-    config.action_links.add :generate_training_assignments, type: :collection, method: :post, page: true
   end
 
   include SpecialitiesTeachingPlansConcern
 
   def generate_training_assignments
-    speciality_ids = params[:id].present?? params[:id] : marked_records.to_a
-    courses = params[:course].present?? [params[:course].to_i] : Group.where(speciality_id: speciality_ids).map(&:course).uniq.sort
-    teaching_plans = TeachingPlan.where(speciality_id: speciality_ids, semester: current_semester.number)
-    discipline_ids = teaching_plans.uniq.pluck(:discipline_id)
-    disciplines = Discipline.reorder(:name).includes(:department).find(discipline_ids)
-    TrainingAssignment.transaction do
-      created = 0
-      courses.each do |course|
-        groups = Group.where(speciality_id: speciality_ids).select{|g| g.course_in(current_semester) == course}
-        teaching_plans.where(course: course).each do |plan|
-          if plan.lections
-            conditions = {weeks_quantity: 18, hours: plan.lections, lesson_type_id: 1, semester_id: current_semester.id, groups: {id: groups.map(&:id)}, disciplines: {id: [plan.discipline_id]}}
-            lec = TrainingAssignment.where(conditions).first_or_initialize
-            unless lec.persisted?
-              lec.groups = groups
-              lec.disciplines = [plan.discipline]
-              lec.save and (created += 1)
-            end
-          end
-          [[:practics, 2], [:lab_works, 3]].each do |type, type_id|
-            groups.each do |group|
-              conditions = {weeks_quantity: 18, hours: plan.send(type), lesson_type_id: type_id, semester_id: current_semester.id, groups: {id: [group.id]}, disciplines: {id: [plan.discipline_id]}}
-              tassign = TrainingAssignment.where(conditions).first_or_initialize
-              unless tassign.persisted?
-                tassign.groups = [group]
-                tassign.disciplines = [plan.discipline]
-                tassign.save and (created += 1)
-              end
-            end
-          end
-        end
-      end
-      each_marked_record {|r| r.as_marked = false} if marked_records.any?
-      redirect_to faculty_training_assignments_path(current_faculty), :notice => "Создано записей: #{created}"
-    end
+    created_count = create_training_assignments
+    redirect_to faculty_training_assignments_path(current_faculty), :notice => "Создано записей: #{created_count}"
   end
 
   def beginning_of_chain
