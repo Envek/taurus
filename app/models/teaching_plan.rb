ad37af1eee18003a2abd95eeff30e81e1b74c4d4
Novikov Andrey <envek@envek.name>
2013-10-31 13:11:50 +0400

    Fix teaching plan has many groups association

--- app/models/teaching_plan.rb
+++ app/models/teaching_plan.rb
@@ -2,7 +2,7 @@
 class TeachingPlan < ActiveRecord::Base
   belongs_to :speciality
   belongs_to :discipline
-  has_many   :groups, through: :speciality, conditions: -> { {forming_year: forming_year} }
+  has_many   :groups, through: :speciality, conditions: proc { {forming_year: forming_year} }
 
   validates_presence_of :speciality_id, :discipline_id, :course, :semester, :forming_year
   validates_numericality_of :course, :semester, :forming_year
